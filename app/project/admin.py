6ff747ba8d475067dadd03cf2a42d209e4420dc2
Mario E Valdes-Tresanco <marioe911116@gmail.com>
2026-03-02 23:44:17 -0700

    feat: Update admin interface styles for user and project changelists with shared CSS

--- app/project/admin.py
+++ app/project/admin.py
@@ -10,12 +10,16 @@ from .models import Project
 class ProjectAdmin(admin.ModelAdmin):
     readonly_fields = ("pk", "path", "slug", "created", "path_exists", "created_by")
     list_display = ("pk", "name", "path", "path_exists", "created_by", "created")
+    search_fields = ("name", "slug", "path", "created_by__email")
 
     # inlines = [
     #    MemberShipInline,
     # ]
 
     filter_horizontal = ("users",)
+
+    class Media:
+        css = {"all": ("css/admin-shared-changelist.css",)}
 
 
 admin.site.register(Project, ProjectAdmin)
