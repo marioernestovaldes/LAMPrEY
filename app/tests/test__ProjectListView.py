995f3262b9ff191e72c7932b9f4af213e4aaa342
Mario E Valdes-Tresanco <marioe911116@gmail.com>
2026-03-31 13:24:38 -0600

    feat: Add truncation and tooltip for long project and pipeline descriptions in tables

--- app/tests/test__ProjectListView.py
+++ app/tests/test__ProjectListView.py
@@ -21,3 +21,18 @@ class ProjectListViewTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Project 1")
         self.assertContains(response, "Project 2")
+
+    def test_long_project_description_is_truncated_in_table(self):
+        Project.objects.create(
+            name="Project 3",
+            description="Long project description. " * 20,
+            created_by=self.user,
+        )
+
+        self.client.force_login(self.user)
+        url = reverse("project:list")
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'class="table-description-text"', html=False)
+        self.assertContains(response, 'title="Long project description.', html=False)
